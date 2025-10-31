9efd05bc562b745300c59697b888d853b23dacda
pongsapakT <141397280+pongsapakT@users.noreply.github.com>
2025-10-31 16:36:01 +0700

    Update settings.py

--- itworkout/itworkout/settings.py
+++ itworkout/itworkout/settings.py
@@ -137,12 +137,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'https://itworkout.s3.amazonaws.com/static/'
+STATIC_URL = 'https://itworkout.s3.us-east-1.amazonaws.com/static/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-MEDIA_URL = 'https://itworkout.s3.amazonaws.com/image/'
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = 'https://itworkout.s3.us-east-1.amazonaws.com/image/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'image')
 
