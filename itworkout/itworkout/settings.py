a91a051a43bac9017a18dbef141d034d1abea440
pongsapakT <141397280+pongsapakT@users.noreply.github.com>
2025-10-31 17:23:06 +0700

    Update settings.py

--- itworkout/itworkout/settings.py
+++ itworkout/itworkout/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-f0+tr@c8n=)z97x=#!gl)*yx)c(27$dwq)j-szv8aig7@1s#tq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["44.220.162.225"]
+ALLOWED_HOSTS = ["54.165.80.189"]
 
 
 # Application definition
