37c6d5e759e9aa4fee3bac5007ad2d3806992567
pongsapakT <141397280+pongsapakT@users.noreply.github.com>
2025-10-31 13:43:59 +0700

    Update settings.py

--- itworkout/itworkout/settings.py
+++ itworkout/itworkout/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         "NAME": "itworkout",
         "USER": "postgres",
         "PASSWORD": "password",
-        "HOST": "database-2.crz1yra97fks.us-east-1.rds.amazonaws.com",
+        "HOST": "database-1.cbtjmytkqigs.us-east-1.rds.amazonaws.com",
         "PORT": "5432",
       }
   }
